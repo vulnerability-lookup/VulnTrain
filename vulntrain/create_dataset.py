0ea1f93c51eee09fa6c04457eeffd5eef6e79f65
Cédric Bonhomme <cedric@cedricbonhomme.org>
2025-02-19 18:54:08 +0100

    chg: [dataset-generation] Improved enrichment of the dataset.

--- vulntrain/create_dataset.py
+++ vulntrain/create_dataset.py
@@ -1,9 +1,10 @@
 """
-    Creates a text/description dataset using vulnerabilities description from Vulnerability-Lookup.
+Creates a text/description dataset using vulnerabilities description from Vulnerability-Lookup.
 
-    Author: Cédric Bonhomme / CIRCL
+Author: Cédric Bonhomme / CIRCL
 
 """
+
 import json
 from typing import Any, Generator
 
@@ -19,25 +20,28 @@ class VulnExtractor:
             decode_responses=True,
         )
 
-
-    def get_vulnerability_meta(self, vulnerability_id: str) -> dict[str, str | dict[str, Any]]:
+    def get_vulnerability_meta(
+        self, vulnerability_id: str
+    ) -> dict[str, str | dict[str, Any]]:
         _vid = vulnerability_id.lower()
         to_return: dict[str, str | dict[str, Any]] = {}
-        for meta_name, meta_uuid in self.valkey_client.hgetall(f'{_vid}:meta').items():
-            if self.valkey_client.exists(f'{meta_name}:{meta_uuid}'):
-                if self.valkey_client.type(f'{meta_name}:{meta_uuid}') == 'string':  # type: ignore[no-untyped-call]  # noqa
-                    if _meta_str := self.valkey_client.get(f'{meta_name}:{meta_uuid}'):
+        for meta_name, meta_uuid in self.valkey_client.hgetall(f"{_vid}:meta").items():
+            if self.valkey_client.exists(f"{meta_name}:{meta_uuid}"):
+                if self.valkey_client.type(f"{meta_name}:{meta_uuid}") == "string":  # type: ignore[no-untyped-call]  # noqa
+                    if _meta_str := self.valkey_client.get(f"{meta_name}:{meta_uuid}"):
                         to_return[meta_name] = _meta_str
-                elif self.valkey_client.type(f'{meta_name}:{meta_uuid}') == 'hash':  # type: ignore[no-untyped-call]  # noqa
-                    if _meta_hash := self.valkey_client.hgetall(f'{meta_name}:{meta_uuid}'):
+                elif self.valkey_client.type(f"{meta_name}:{meta_uuid}") == "hash":  # type: ignore[no-untyped-call]  # noqa
+                    if _meta_hash := self.valkey_client.hgetall(
+                        f"{meta_name}:{meta_uuid}"
+                    ):
                         to_return[meta_name] = _meta_hash
             else:
-                print(f'Unable to find meta {meta_uuid} for {meta_name}')
+                print(f"Unable to find meta {meta_uuid} for {meta_name}")
         return to_return
 
-
-    def get_vulnerability(self, vulnerability_id: str,
-                          *, with_meta: bool | None=False) -> dict[str, Any] | None:
+    def get_vulnerability(
+        self, vulnerability_id: str, *, with_meta: bool | None = False
+    ) -> dict[str, Any] | None:
         _vid = vulnerability_id.lower()
         _vuln = self.valkey_client.get(_vid)
         if not _vuln:
@@ -45,31 +49,59 @@ class VulnExtractor:
         vuln = json.loads(_vuln)
         if with_meta:
             if meta := self.get_vulnerability_meta(_vid):
-                vuln['vulnerability-lookup:meta'] = meta
+                vuln["vulnerability-lookup:meta"] = meta
         return vuln
 
-    def get_all(self, source: str = "", /, with_meta: bool=False) -> Generator[dict[str, Any], None, None]:
+    def get_all(
+        self, source: str = "", /, with_meta: bool = False
+    ) -> Generator[dict[str, Any], None, None]:
         """This method will scan a complete source and yield the vulnerabilities.
         It is up to the caller to handle the yielded entries as it will be a lot"""
         if source:
-            key = f'index:{source}'
+            key = f"index:{source}"
         else:
-            key = 'index'
+            key = "index"
         for vuln_id, _ in self.valkey_client.zscan_iter(key):
             if vuln := self.get_vulnerability(vuln_id, with_meta=with_meta):
                 yield vuln
-    
-    def __call__(self):
-        count = 0
-        for vuln in self.get_all("nvd", True):
-            count += 1
-            if count == 10000:
-                return
 
+    def __call__(self):
+        # count = 0
+        for vuln in self.get_all("cvelistv5", True):
+            # count += 1
+            # if count == 1000:
+            #     return
+
+            #
+            # CVE id, title, and description
+            #
+            vuln_id = vuln["cveMetadata"]["cveId"]
+            vuln_title = vuln["containers"]["cna"].get("title", "")
+            # if not vuln_title:
+            #     for entry in vuln["containers"].get("adp", []):
+            #         if "title" in entry:
+            #             vuln_title = entry.get("title")
+            #             break
+            #     else:
+            #         vuln_title = ""
+            for description in vuln["containers"]["cna"].get("descriptions", []):
+                if description["lang"].lower() in ["en", "en-en", "en-us"]:
+                    vuln_description = description["value"]
+                    break
+            else:
+                continue
+
+            #
+            # CPE
+            #
+
+            # vulnrichement
             vuln_cpes = []
-            if vulnrichment := vuln["vulnerability-lookup:meta"].get("vulnrichment", False):
+            if vulnrichment := vuln["vulnerability-lookup:meta"].get(
+                "vulnrichment", False
+            ):
                 containers = json.loads(vulnrichment["containers"])
-    
+
                 # Check ADP section
                 if "adp" in containers:
                     for entry in containers["adp"]:
@@ -77,44 +109,37 @@ class VulnExtractor:
                             for affected in entry["affected"]:
                                 if "cpes" in affected:
                                     vuln_cpes.extend(affected["cpes"])
-                
+
                 # Check CNA section
                 if "cna" in containers and "affected" in containers["cna"]:
                     for affected in containers["cna"]["affected"]:
                         if "cpes" in affected:
                             vuln_cpes.extend(affected["cpes"])
 
-                # for elem in vulnrichment["containers"]["adp"]["affected"]:
-                #     if "cpes" in elem:
-                #         print(elem)
-                #         vuln_cpes = elem["cpes"]
-                #         break
-                #     else:
-                #         vuln_cpes = []
+            # fkie
+            if fkie := vuln["vulnerability-lookup:meta"].get("fkie_nvd", False):
+                if "configurations" in fkie:
+                    configurations = json.loads(fkie["configurations"])
+                    for config in configurations:
+                        if "nodes" in config:
+                            for node in config["nodes"]:
+                                if "cpeMatch" in node:
+                                    vuln_cpes.extend(
+                                        match["criteria"]
+                                        for match in node["cpeMatch"]
+                                        if "criteria" in match
+                                    )
 
-            vuln_id = vuln["cveMetadata"]["cveId"]
+            # default container
+            for elem in vuln["containers"]["cna"]["affected"]:
+                if "cpes" in elem:
+                    vuln_cpes.extend(elem["cpes"])
 
-            vuln_title = vuln["containers"]["cna"].get("title", "")
+            vuln_cpes = list(dict.fromkeys(cpe.lower() for cpe in vuln_cpes))
 
-            for description in vuln["containers"]["cna"].get("descriptions", []):
-                if description["lang"].lower() in ["en", "en-sn", "en-us"]:
-                    vuln_description = description["value"]
-                    break
-            else:
-                continue
-
-            # if not vuln_cpes:
-            #     for elem in vuln["containers"]["cna"]["affected"]:
-            #         if "cpes" in elem:
-            #             vuln_cpes = elem["cpes"]
-            #             break
-            #     else:
-            #         vuln_cpes = []
-
-
-
-            # print(vuln_cpes)
-
+            #
+            # Create the data
+            #
             vuln_data = {
                 "id": vuln_id,
                 "title": vuln_title,
@@ -122,9 +147,6 @@ class VulnExtractor:
                 "cpes": vuln_cpes,
             }
             yield vuln_data
-
-
-
 
 
 def main():
--- vulntrain/utils.py
+++ vulntrain/utils.py
@@ -1,5 +1,6 @@
 from nltk.tokenize import sent_tokenize
 
+
 def sentences(text, num_sentences=5):
     sentences = sent_tokenize(text)[:num_sentences]
     return " ".join(sentences)
