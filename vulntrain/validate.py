962ff0e59c1820d9f009688a66524133f36782b8
Cédric Bonhomme <cedric@cedricbonhomme.org>
2025-02-20 09:38:50 +0100

    chg: [validation] Added a simple validation script for model optimized for text generation.

---
+++ vulntrain/validate.py
@@ -0,0 +1,32 @@
+import argparse
+
+from transformers import pipeline  # type: ignore[import-untyped]
+
+def main():
+    """Instantiates a generator based on a model optimized for text generation and
+    send a task (prompt) to the model.
+    """
+    parser = argparse.ArgumentParser(description="Validate a text generation model for vulnerabilities.")
+    parser.add_argument(
+        "--model",
+        dest="model",
+        help="The model to use.",
+        default="CIRCL/vuln-model-test",
+    )
+    parser.add_argument(
+        "--prompt",
+        dest="prompt",
+        help="The prompt for the generator.",
+        default="A new vulnerability in OpenSSL allows attackers to",
+    )
+
+    args = parser.parse_args()
+
+    # Load the model from Hugging Face
+    generator = pipeline("text-generation", model=args.model)
+
+    print(generator(args.prompt, max_length=100))
+
+
+if __name__ == "__main__":
+    main()
