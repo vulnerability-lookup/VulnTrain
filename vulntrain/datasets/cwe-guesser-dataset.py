fe1db1301f08ebdbfbc4535e1c3293bc5fa2c3fb
lea <hamartia.eu@gmail.com>
2025-07-17 14:32:52 +0200

    fix: [dataset] took out dependencies on valkey to only use the api call

--- vulntrain/datasets/cwe-guesser-dataset.py
+++ vulntrain/datasets/cwe-guesser-dataset.py
@@ -2,19 +2,11 @@ import argparse
 import json
 from typing import Any, Generator
 
-import valkey
 from datasets import Dataset, DatasetDict
-
 import requests
-import os
-import time
 
-from vulntrain.config import valkey_host, valkey_port
 from vulntrain.utils import (
     strip_markdown,
-    extract_cpe,
-    extract_cpe_csaf,
-    extract_cvss_cve,
     extract_cvss_from_github_advisory,
     extract_cvss_from_pysec,
     extract_cvss_from_csaf,
@@ -24,42 +16,8 @@ class VulnExtractor:
     def __init__(self, sources: list[str], nb_rows: int):
         self.sources = sources
         self.nb_rows = nb_rows
-        self.valkey_client = valkey.Valkey(
-            host=valkey_host, port=valkey_port, decode_responses=True
-        )
 
-    def get_vulnerability_meta(self, vulnerability_id: str) -> dict[str, Any]:
-        _vid = vulnerability_id.lower()
-        metadata = {}
-
-        for meta_name, meta_uuid in self.valkey_client.hgetall(f"{_vid}:meta").items():
-            key = f"{meta_name}:{meta_uuid}"
-            if self.valkey_client.exists(key):
-                meta_type = self.valkey_client.type(key)
-                if meta_type == "string":
-                    metadata[meta_name] = self.valkey_client.get(key)
-                elif meta_type == "hash":
-                    metadata[meta_name] = self.valkey_client.hgetall(key)
-            else:
-                print(f"Warning: Unable to find meta {meta_uuid} for {meta_name}")
-
-        return metadata
-
-    def get_vulnerability(
-        self, vulnerability_id: str, with_meta: bool = False
-    ) -> dict[str, Any] | None:
-        _vid = vulnerability_id.lower()
-        _vuln = self.valkey_client.get(_vid) 
-        if not _vuln:
-            return None
-
-        vuln = json.loads(_vuln)
-        if with_meta:
-            vuln["vulnerability-lookup:meta"] = self.get_vulnerability_meta(_vid)
-
-        return vuln
-
-    def get_all( self, source: str, with_meta: bool = False ) -> Generator[dict[str, Any], None, None]:
+    def get_all(self, source: str, with_meta: bool = False) -> Generator[dict[str, Any], None, None]:
         page = 1
         per_page = 1000
 
@@ -74,10 +32,6 @@ class VulnExtractor:
                     break  # Stop if empty page
 
                 for vuln in data:
-                    if with_meta and 'id' in vuln:
-                        vid = vuln['id']
-                        meta = self.get_vulnerability_meta(vid)
-                        vuln["vulnerability-lookup:meta"] = meta
                     yield vuln
 
                 page += 1  
@@ -86,7 +40,6 @@ class VulnExtractor:
                 print(f"Error fetching page {page} from source '{source}': {e}")
                 break
 
-         
     def is_url_alive(self, url: str, timeout: int = 5) -> bool:
         try:
             response = requests.head(url, timeout=timeout, allow_redirects=True)
@@ -94,52 +47,53 @@ class VulnExtractor:
         except requests.RequestException:
             return False
 
+    def filter_alive_links(self, urls: list[str]) -> list[str]:
+        return [url for url in urls if self.is_url_alive(url)]
+
     def extract_cve(self, vuln: dict[str, Any]) -> dict[str, Any]:
-        vuln_id = vuln["cveMetadata"]["cveId"]
-        vuln_title = vuln["containers"]["cna"].get("title", "")
-        vuln_description = next(
-            (
-                desc["value"]
-                for desc in vuln["containers"]["cna"].get("descriptions", [])
-                if desc["lang"].startswith("en")
-            ),
-            "",
-        )
+        try:
+            vuln_id = vuln["cveMetadata"]["cveId"]
+            vuln_title = vuln["containers"]["cna"].get("title", "")
+            vuln_description = next(
+                (
+                    desc["value"]
+                    for desc in vuln["containers"]["cna"].get("descriptions", [])
+                    if desc["lang"].startswith("en")
+                ),
+                "",
+            )
 
-        patch_references = [
-            ref.get("url", "")
-            for ref in vuln["containers"]["cna"].get("references", [])
-            if "tags" in ref and "patch" in ref["tags"]
-            and self.is_url_alive(ref.get("url", ""))
-        ]
-        if not patch_references:
+            patch_references = [
+                ref.get("url", "")
+                for ref in vuln["containers"]["cna"].get("references", [])
+                if "tags" in ref and "patch" in ref["tags"]
+                and self.is_url_alive(ref.get("url", ""))
+            ]
+            print(f"Found {len(patch_references)} patch references for {vuln_id}")
+            if not patch_references or not vuln_description:
+                return {}
+
+            cwe_id = ""
+            cwe_desc = ""
+            problem_types = vuln["containers"]["cna"].get("problemTypes", [])
+            if problem_types:
+                descriptions = problem_types[0].get("descriptions", [])
+                if descriptions:
+                    cwe_id = descriptions[0].get("cweId", "")
+                    cwe_desc = descriptions[0].get("description", "")
+
+            return {
+                "id": vuln_id,
+                "title": vuln_title,
+                "description": vuln_description,
+                "references": patch_references,
+                "cwe_id": cwe_id,
+                "cwe_description": cwe_desc,
+            }
+        except KeyError:
             return {}
-        
-        cwe_id = ""
-        cwe_desc = ""
-        problem_types = vuln["containers"]["cna"].get("problemTypes", [])
-        if problem_types:
-            descriptions = problem_types[0].get("descriptions", [])
-            if descriptions:
-                cwe_id = descriptions[0].get("cweId", "")
-                cwe_desc = descriptions[0].get("description", "")
-
-        if not vuln_description:
-            return {}
-
-        return {
-            "id": vuln_id,
-            "title": vuln_title,
-            "description": vuln_description,
-            "references": patch_references,
-            "cwe_id": cwe_id,
-            "cwe_description": cwe_desc,
-        }
-
-
 
     def extract_ghsa(self, vuln: dict[str, Any]) -> dict[str, Any]:
-        cvss_scores = extract_cvss_from_github_advisory(vuln)
         references = vuln.get("references", [])
 
         patch_references = [
@@ -160,9 +114,7 @@ class VulnExtractor:
             "patch_links": patch_references,
         }
 
-
     def extract_pysec(self, vuln: dict[str, Any]) -> dict[str, Any]:
-        cvss_scores = extract_cvss_from_pysec(vuln)
         references = vuln.get("references", [])
 
         patch_references = [
@@ -179,10 +131,7 @@ class VulnExtractor:
             "patch_links": patch_references,
         }
 
-
     def extract_csaf(self, vuln: dict[str, Any]) -> dict[str, Any]:
-        cvss_scores = extract_cvss_from_csaf(vuln)
-
         description = " ".join(
             [
                 note["text"]
@@ -223,66 +172,63 @@ class VulnExtractor:
         return {
             "id": vuln["document"]["tracking"]["id"],
             "title": vuln["document"]["title"],
-            # "description": description,
+            "description": description,
             "patch_links": patch_references,
-            "cwes": cwes,   
+            "cwes": cwes,
         }
-
 
     def __call__(self) -> Generator[dict[str, Any], None, None]:
         count = 0
-        for source in self.sources:
-            match source:
-                case "cvelistv5":
-                    extractor = self.extract_cve
-                case "github":
-                    extractor = self.extract_ghsa
-                case "pysec":
-                    extractor = self.extract_pysec
-                case str() as s if s.startswith("csaf_"):
-                    extractor = self.extract_csaf
-                case _:
-                    print("No parser for this source.")
-                    continue
+        with open("vulns_success.jsonl", "w") as success_file, open("vulns_error.jsonl", "w") as error_file:
+            for source in self.sources:
+                match source:
+                    case "cvelistv5":
+                        extractor = self.extract_cve
+                    case "github":
+                        extractor = self.extract_ghsa
+                    case "pysec":
+                        extractor = self.extract_pysec
+                    case str() as s if s.startswith("csaf_"):
+                        extractor = self.extract_csaf
+                    case _:
+                        print("No parser for this source.")
+                        continue
 
-            for vuln in self.get_all(source, True):
-                vuln_data = extractor(vuln)
+                for vuln in self.get_all(source, with_meta=False):
+                    try:
+                        vuln_data = extractor(vuln)
 
-                if not vuln_data.get("description"):
-                    continue
+                        if not vuln_data or not vuln_data.get("description"):
+                            error_file.write(json.dumps(vuln) + "\n")
+                            continue
 
-                yield vuln_data
+                        success_file.write(json.dumps(vuln_data) + "\n")
+                        yield vuln_data
 
-                count += 1
-                if count == self.nb_rows:
-                    return
-             
-    
+                        count += 1
+                        if self.nb_rows and count >= self.nb_rows:
+                            return
+                    except Exception as e:
+                        print(f"Error processing vulnerability: {e}")
+                        error_file.write(json.dumps(vuln) + "\n")
+
 def main():
     parser = argparse.ArgumentParser(description="Dataset generation.")
     parser.add_argument(
         "--sources",
         required=True,
-        help="Comma-separated list of sources (cvelistv5, github, pysec)",
+        help="Comma-separated list of sources (cvelistv5, github, pysec, csaf_xxx)",
     )
     parser.add_argument(
-        "--nb-rows", type=int, default=0, help="Number of rows in the dataset"
+        "--nb-rows", type=int, default=0, help="Number of rows in the dataset (0 = all)"
     )
 
     args = parser.parse_args()
 
     sources = args.sources.split(",")
     extractor = VulnExtractor(sources, args.nb_rows)
-    vulns = list(extractor())
 
-    dataset = Dataset.from_list(vulns)
-    dataset_dict = dataset.train_test_split(test_size=0.1)
-
-    dataset_dict = DatasetDict(
-        {"train": dataset_dict["train"], "test": dataset_dict["test"]}
-    )
-    print(dataset_dict)
-
+    list(extractor())  
 
 if __name__ == "__main__":
     main()
