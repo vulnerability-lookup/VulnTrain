9e0eba89814dd66ddd9d75bf43d4e57af64ddbc7
lea <hamartia.eu@gmail.com>
2025-08-08 14:49:26 +0200

    chg: [trainer] also test

---
+++ vulntrain/trainers/benchmark_models.py
@@ -0,0 +1,29 @@
+import subprocess
+
+# List of base models to test
+base_models = [
+    "bert-base-uncased",
+    "roberta-base",
+    "distilbert-base-uncased",
+    "microsoft/codebert-base",
+    "microsoft/graphcodebert-base",
+    "huggingface/CodeBERTa-small-v1",
+]
+
+dataset_id = "CIRCL/vulnerability-cwe-patch"
+hf_repo_prefix = "CIRCL/vuln-patch-cwe-guesser-model" 
+save_dir_base = "results"
+
+for model_name in base_models:
+    print(f"\n==== Running training for: {model_name} ====")
+    
+    repo_id = f"{hf_repo_prefix}-{model_name.replace('/', '-')}"
+    save_dir = f"{save_dir_base}/{model_name.replace('/', '-')}"
+    
+    subprocess.run([
+        "python", "vulntrain/trainers/cwe-guesser-commit-mess.py",
+        "--base-model", model_name,
+        "--dataset-id", dataset_id,
+        "--repo-id", repo_id,
+        "--model-save-dir", save_dir,
+    ])
